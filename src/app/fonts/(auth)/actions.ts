9156aa59f796dcb3e1d891bcd0aba2a091b04b76
Your Name <marcosralbrecht@gmail.com>
2025-04-03 23:16:05 -0300

    feat: verify existing email, username and set cookies

--- src/app/fonts/(auth)/actions.ts
+++ src/app/fonts/(auth)/actions.ts
@@ -1,9 +1,13 @@
 "use server";
 
+import { lucia } from "@/auth";
 import prisma from "@/lib/prisma";
 import { signUpSchema, SignupValues } from "@/lib/validations";
 import { hash } from "@node-rs/argon2";
 import { generateIdFromEntropySize } from "lucia";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export async function signUp(
   credentials: SignupValues,
@@ -28,7 +32,45 @@ export async function signUp(
         },
       },
     });
+
+    if (existingUserName) {
+      return { error: "Username already taken" };
+    }
+
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return { error: "Email already taken" };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        email,
+        username,
+        displayName: username,
+        passwordHash,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
   } catch (error) {
+    if (isRedirectError(error)) throw error;
     console.error("Error signing up:", error);
     return { error: "An error occurred during sign up. Please try again." };
   }
